f0fed614703ebb72c1fd27f4f43a976177052a52
Rick Carlino <rick.carlino@gmail.com>
2014-12-27 10:18:32 -0600

    Done with tracker controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: :show
   namespace :api, defaults: {format: 'json'} do
-    resources :trackers, only: [:index, :create, :update, :destroy]
+    resources :trackers, only: [:index, :create, :update, :destroy] do
+      resources :events, only: [:create]
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
